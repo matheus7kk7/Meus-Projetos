fd0ec63e2bb1a3ee3ef831b68e8f6aa2c8089a83
matheus7kk7 <matheus7kifba@gmail.com>
2024-04-15 12:02:49 -0300

    Criei um arquivo de estilo css e o linkei a pasta index urilizando um corpo de html

--- index.php
+++ index.php
@@ -1,13 +1,30 @@
-<?php
-function soma($n1, $n2)
-{
-    return $n1 + $n2;
-}
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+
+    <title>Home </title>
+</head>
+
+<body>
 
 
-$num1 = 3;
-$num2 = 3;
+    <?php
+    function soma($n1, $n2)
+    {
+        return $n1 + $n2;
+    }
 
-echo soma($num1, $num2);
 
-?>
+    $num1 = 3;
+    $num2 = 3;
+
+    echo soma($num1, $num2);
+
+    ?>
+</body>
+
+</html>
